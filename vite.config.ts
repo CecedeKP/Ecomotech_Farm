5744d748258142ac7f6d777932694b476a67479a
CecedeKP <99571735+Bamamou@users.noreply.github.com>
2025-04-04 18:34:08 +0800

    Need more test to confirm

--- vite.config.ts
+++ vite.config.ts
@@ -13,10 +13,15 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
-    rollupOptions: {
-      output: {
-        manualChunks: undefined,
-      },
-    },
   },
+  server: {
+    port: 3000,
+    open: true,
+    host: true
+  },
+  preview: {
+    port: 4173,
+    open: true,
+    host: true
+  }
 })
